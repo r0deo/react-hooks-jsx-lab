b5c0eb8d7fa90d2a8581b0cb578f0ac8a9dd8b8d
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-03-10 11:55:36 +0300

    assignment completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,16 @@
 import React from "react";
+import Home from "./Home";
+import About from "./About";
 
 function NavBar() {
-  // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+    <div>
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+      </nav>
+    </div>
+  );
 }
 
 export default NavBar;
